9c37dba41c9020f545f16f7fcc9fa6e7ee5b960c
Mohamed Yusuf <mohamed208.y@gmail.com>
2024-07-15 11:42:18 +0300

    API works

--- backend_flask_api/main.py
+++ backend_flask_api/main.py
@@ -3,13 +3,11 @@ from flask_cors import CORS, cross_origin
 import json
 import requests
 
-
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
 post_json = {}
-
 
 @app.route("/")
 def hello_world():
@@ -20,10 +18,9 @@ def hello_world():
 @cross_origin()
 def data():
     if request.method == "POST":
-        data = request.json
-        extract(data)
-        print(post_json)
-
+        data = extract(request.json)
+        return data
+    
     return '200'
 
 
@@ -31,20 +28,17 @@ def extract(excel_data):
     ip_list = extract_values(excel_data, "IPV4")
     urls = extract_values(excel_data, "FQDN")
 
+    for i in range(len(urls)):
+        if type(urls[i]) == str:
+            urls[i] = urls[i].replace("[.]", ".")
+
     dates = []
     countries = []
     country_name = []
     abuse = []
     key = "9caf023f75484c2315dc7cac2fa8f980e2728d1a0f69ccdc679f722c694185349e82b4be5e20c76c"
 
-    url = "https://api.abuseipdb.com/api/v2/check"
-    querystring = {"ipAddress": "118.25.6.39", "maxAgeInDays": "90"}
-    headers = {"Accept": "application/json", "Key": key}
-
-    response = requests.request(
-        method="GET", url=url, headers=headers, params=querystring
-    )
-    decodedResponse = json.loads(response.text)
+    fused_lists = []
 
     for a in range(0, len(ip_list)):
         ip_list[a] = ip_list[a].replace("[.]", ".")
@@ -59,7 +53,7 @@ def extract(excel_data):
         response_dict = json.loads(response.text)
 
         try:
-            countries.append(response_dict["data"]["countryCode"])
+            fused_lists.append({"country": response_dict["data"]["countryCode"], "abuse": response_dict["data"]["abuseConfidenceScore"]})
         except:
             nothing()
 
@@ -78,24 +72,23 @@ def extract(excel_data):
         except:
             nothing()
 
-    for i in range(len(urls)):
-        if type(urls[i]) == str:
-            urls[i] = urls[i].replace("[.]", ".")
-
-    fused_lists = {}
-    fused_lists["countries"] = countries
-    fused_lists["country_name"] = country_name
-    fused_lists["dates"] = dates
-    fused_lists["abuse"] = abuse
-
-    post_json = fused_lists;
-    #FIGURE OUT A GOOD WAY OF STORING THIS INFO
-
-    print("Success: data.json is now in your publics folder")
+    print(fused_lists)
     return fused_lists
 
 def extract_values(obj_list, key):
-    return [obj[key] for obj in obj_list if key in obj]
+    return_list = []
+
+    if type(obj_list) is list:
+        for item in obj_list:
+            try:
+                return_list.append(item[key])
+            except:
+                print(f'{key} not found')
+
+        return return_list
+
+    print('check failed')
+    return {}
 
 def nothing():
     return 0
