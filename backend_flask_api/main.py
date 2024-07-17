480b2d1f82db95aebd85b40bce6b7f1c30fa5ca6
Mohamed Yusuf <mohamed208.y@gmail.com>
2024-07-17 10:39:09 +0300

    Geodata hydration works

--- backend_flask_api/main.py
+++ backend_flask_api/main.py
@@ -61,6 +61,46 @@ def make_abuse_countries(excel_data):
 
     return fused_lists
 
+def make_geo_countries(excel_data): #use this in case you need for API.
+    if type(excel_data) is not list:
+        return '205'
+
+    ip_list = extract_values(excel_data, "IPV4") 
+    urls = extract_values(excel_data, "FQDN") 
+
+    dates = []
+    countries = []
+    country_name = []
+    abuse = []
+    #key = "9caf023f75484c2315dc7cac2fa8f980e2728d1a0f69ccdc679f722c694185349e82b4be5e20c76c"
+    key = "9e19a670e5a990c979aef2cd3f66e0a5185d2cdfb3db534d6b5a7b5f7573b35aacb64ac6bf88ac4f"
+
+    fused_lists = []
+
+    for a in range(0, len(ip_list)):
+        ip_list[a] = ip_list[a].replace("[.]", ".")
+
+        url = "https://api.abuseipdb.com/api/v2/check"
+        querystring = {"ipAddress": ip_list[a], "maxAgeInDays": "90"}
+        headers = {"Accept": "application/json", "Key": key}
+
+        response = requests.request(
+            method="GET", url=url, headers=headers, params=querystring
+        )
+
+        response_dict = json.loads(response.text)
+        print(response_dict)
+       
+        try:
+            fused_lists.append(
+                {"country": response_dict["data"]["countryCode"], "abuse": response_dict["data"]["abuseConfidenceScore"],}
+            )
+        except Exception:
+           print('Failed to add item to the return list') 
+
+
+
+
 def extract_values(obj_list, key):
     return_list = []
 
@@ -70,6 +110,7 @@ def extract_values(obj_list, key):
                 return_list.append(item[key])
 
         return return_list
+
 def clean_urls(urls):
     for i in range(len(urls)):
             if type(urls[i]) == str:
